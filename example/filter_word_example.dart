4135b0ab23eebf29309e95c521758920e5bd7424
ichsanputr <githubichsan@gmail.com>
2022-12-22 09:01:05 +0800

    init package

---
+++ example/filter_word_example.dart
@@ -0,0 +1,6 @@
+import 'package:filter_word/filter_word.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/filter_word.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library filter_word;
+
+export 'src/filter_word_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
